d21972c51d639bccdd3ae48519cadef1df9c2fe9
Steven Wen <73114262+loopfree@users.noreply.github.com>
2021-10-28 21:58:34 +0700

    minor update
    menambah deskripsi pada fungsi compress

--- compress.py
+++ compress.py
@@ -1,14 +1,13 @@
 # image = directory foto => "Folder_ini/static/ini_foto.png"
 # compression_rate = xx%
 
+# Fungsi compress ini nantinya mengkompress foto
+# lalu hasilnya disimpan dalam folder static
+# dan me-return directory dari foto tersebut
 def compress(image, compression_rate):
-	print("Start")
-	print(compression_rate)
-	print(image)
-	print("End")
+	print("Start")				# Start
+	print(compression_rate)		# 70
+	print(image)				# static\lenna.png
+	print("End")				# End
 
-	# Sample Output
-	# Start
-	# 70
-	# static\lenna.png
-	# End
+	# return("static/hasil.png")
